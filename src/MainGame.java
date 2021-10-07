0882237b28dda5bcab0b20796bdb83fa11dd391d
Raydelto Hernandez <rhernandez@dynascape.com>
2021-10-07 17:16:27 -0400

    Refactoring the GUI (#3)

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,14 +1,5 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class MainGame extends JPanel {
+public class MainGame{
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("Ludo");
-
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
+        new MainWindow();
     }
 }
